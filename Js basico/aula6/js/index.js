519a8eb55049994c5bb38d85bad3610757dfa1af
alirod1997 <alisonguns2011@hotmail.com>
2024-08-06 23:54:39 -0300

    resolução do exercicio aula 6 concatenação

--- Js basico/aula6/js/index.js
+++ Js basico/aula6/js/index.js
@@ -1,3 +1,4 @@
+//Maneiras de Resolução do exercicio:
 const nome = 'Alisson';
 const sobrenome = 'Rodrigues Ferreira';
 const idade = 27;
@@ -6,6 +7,17 @@ const altura = 1.73;
 let imc = peso/ (altura * altura); 
 let anoNascimento = 2024 - idade;
 
-console.log(nome +' '+sobrenome+ ' tem '+idade+' anos, pesa '+peso+'kg.');
+//Concatenando/unindo com sinal de +
+console.log(nome +' '+sobrenome+ ' tem '+idade+' anos, pesa '+peso+' kg.');
 console.log('tem '+altura+' de altura e seu IMC é de '+ imc);
 console.log(nome +' '+ sobrenome + ' nasceu em '+ anoNascimento);
+
+//separando as informações com Virgula
+console.log(nome, sobrenome, 'tem ', idade,' anos, pesa ',peso,' kg.');
+console.log('tem',altura,'de altura e seu IMC é de', imc);
+console.log(nome,sobrenome,'nasceu em',anoNascimento);
+
+//Template Strings = Forma mais legível
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa peso kg.`);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}`); 
